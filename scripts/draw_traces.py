415b886cd3c2d7a017a8499e8877b8f99593d9fd
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2019-07-29 20:05:08 +0200

    refactor t_TP into for loop and list

--- scripts/draw_traces.py
+++ scripts/draw_traces.py
@@ -4,11 +4,13 @@ import numpy as np
 import random
 
 def main():
-    np.random.seed(4)
-    random.seed = 4
-
     # load csv as data frame
     df = pd.read_csv('../example_data/draw_traces/top5_correlated.csv')
+
+    frames_recorded = 6080
+
+    # calculate number of time points
+    ntimepoints = int(len(df) // frames_recorded)
 
     # extract time (t) and fluorescence data for each ROI (cell 1-5)
     t = df.ix[:,'y0000']
@@ -19,10 +21,10 @@ def main():
     cell5 = df.ix[:,'x0004']
 
     #extract time (t) for 4 timepoints (TP)
-    t_TP1 = t.ix[0:6079]
-    t_TP2 = t.ix[6080:12159]
-    t_TP3 = t.ix[12160:18239]
-    t_TP4 = t.ix[18240:24319]
+    t_TPs = []
+    for timepoint in range(ntimepoints):
+        t_TP = t.ix[timepoint*frames_recorded : (timepoint+1)*frames_recorded]
+        t_TPs.append(t_TP)
 
     # extract data for each time point from ROI 0000 (cell1)
     cell1_TP1 = cell1.ix[0:6079]
@@ -66,19 +68,19 @@ def main():
     axTP1_cell5 = fig_TP1.add_subplot(515)
 
 
-    axTP1_cell1.plot(t_TP1, cell1_TP1, linewidth = 0.25, color = 'firebrick')
+    axTP1_cell1.plot(t_TPs[0], cell1_TP1, linewidth = 0.25, color = 'firebrick')
     axTP1_cell1.set_ylim(3.8,4.8)
     axTP1_cell1.axis('off')
-    axTP1_cell2.plot(t_TP1,cell2_TP1, linewidth = 0.25, color = 'sandybrown')
+    axTP1_cell2.plot(t_TPs[0],cell2_TP1, linewidth = 0.25, color = 'sandybrown')
     axTP1_cell2.set_ylim(3.3,4.3)
     axTP1_cell2.axis('off')
-    axTP1_cell3.plot(t_TP1,cell3_TP1, linewidth = 0.25, color = 'darkkhaki')
+    axTP1_cell3.plot(t_TPs[0],cell3_TP1, linewidth = 0.25, color = 'darkkhaki')
     axTP1_cell3.set_ylim(2,3)
     axTP1_cell3.axis('off')
-    axTP1_cell4.plot(t_TP1,cell4_TP1, linewidth = 0.25, color = 'seagreen')
+    axTP1_cell4.plot(t_TPs[0],cell4_TP1, linewidth = 0.25, color = 'seagreen')
     axTP1_cell4.set_ylim(0.8,1.8)
     axTP1_cell4.axis('off')
-    axTP1_cell5.plot(t_TP1,cell5_TP1, linewidth = 0.25, color = 'royalblue')
+    axTP1_cell5.plot(t_TPs[0],cell5_TP1, linewidth = 0.25, color = 'royalblue')
     axTP1_cell5.set_ylim(-0.2,0.8)
     #axTP1_cell5.axis('off')
 
@@ -108,19 +110,19 @@ def main():
     axTP2_cell4 = fig_TP2.add_subplot(514)
     axTP2_cell5 = fig_TP2.add_subplot(515)
 
-    axTP2_cell1.plot(t_TP2,cell1_TP2, linewidth = 0.25, color = 'firebrick')
+    axTP2_cell1.plot(t_TPs[1],cell1_TP2, linewidth = 0.25, color = 'firebrick')
     axTP2_cell1.set_ylim(4,5)
     axTP2_cell1.axis('off')
-    axTP2_cell2.plot(t_TP2,cell2_TP2, linewidth = 0.25, color = 'sandybrown')
+    axTP2_cell2.plot(t_TPs[1],cell2_TP2, linewidth = 0.25, color = 'sandybrown')
     axTP2_cell2.set_ylim(2.8,3.8)
     axTP2_cell2.axis('off')
-    axTP2_cell3.plot(t_TP2,cell3_TP2, linewidth = 0.25, color = 'darkkhaki')
+    axTP2_cell3.plot(t_TPs[1],cell3_TP2, linewidth = 0.25, color = 'darkkhaki')
     axTP2_cell3.set_ylim(2, 3)
     axTP2_cell3.axis('off')
-    axTP2_cell4.plot(t_TP2,cell4_TP2, linewidth = 0.25, color = 'seagreen')
+    axTP2_cell4.plot(t_TPs[1],cell4_TP2, linewidth = 0.25, color = 'seagreen')
     axTP2_cell4.set_ylim(0.8,1.8)
     axTP2_cell4.axis('off')
-    axTP2_cell5.plot(t_TP2,cell5_TP2, linewidth = 0.25, color = 'royalblue')
+    axTP2_cell5.plot(t_TPs[1],cell5_TP2, linewidth = 0.25, color = 'royalblue')
     axTP2_cell5.set_ylim(0.2,1.2)
     axTP2_cell5.axis('off')
 
@@ -136,19 +138,19 @@ def main():
     axTP3_cell4 = fig_TP3.add_subplot(514)
     axTP3_cell5 = fig_TP3.add_subplot(515)
 
-    axTP3_cell1.plot(t_TP3,cell1_TP3, linewidth = 0.25, color = 'firebrick')
+    axTP3_cell1.plot(t_TPs[2],cell1_TP3, linewidth = 0.25, color = 'firebrick')
     axTP3_cell1.set_ylim(4,5)
     axTP3_cell1.axis('off')
-    axTP3_cell2.plot(t_TP3,cell2_TP3, linewidth = 0.25, color = 'sandybrown')
+    axTP3_cell2.plot(t_TPs[2],cell2_TP3, linewidth = 0.25, color = 'sandybrown')
     axTP3_cell2.set_ylim(2.8,3.8)
     axTP3_cell2.axis('off')
-    axTP3_cell3.plot(t_TP3,cell3_TP3, linewidth = 0.25, color = 'darkkhaki')
+    axTP3_cell3.plot(t_TPs[2],cell3_TP3, linewidth = 0.25, color = 'darkkhaki')
     axTP3_cell3.set_ylim(2, 3)
     axTP3_cell3.axis('off')
-    axTP3_cell4.plot(t_TP3,cell4_TP3, linewidth = 0.25, color = 'seagreen')
+    axTP3_cell4.plot(t_TPs[2],cell4_TP3, linewidth = 0.25, color = 'seagreen')
     axTP3_cell4.set_ylim(0.8,1.8)
     axTP3_cell4.axis('off')
-    axTP3_cell5.plot(t_TP3,cell5_TP3, linewidth = 0.25, color = 'royalblue')
+    axTP3_cell5.plot(t_TPs[2],cell5_TP3, linewidth = 0.25, color = 'royalblue')
     axTP3_cell5.set_ylim(0.2,1.2)
     axTP3_cell5.axis('off')
 
@@ -164,19 +166,19 @@ def main():
     axTP4_cell4 = fig_TP4.add_subplot(514)
     axTP4_cell5 = fig_TP4.add_subplot(515)
 
-    axTP4_cell1.plot(t_TP4,cell1_TP4, linewidth = 0.25, color = 'firebrick')
+    axTP4_cell1.plot(t_TPs[3],cell1_TP4, linewidth = 0.25, color = 'firebrick')
     axTP4_cell1.set_ylim(4,5)
     axTP4_cell1.axis('off')
-    axTP4_cell2.plot(t_TP4,cell2_TP4, linewidth = 0.25, color = 'sandybrown')
+    axTP4_cell2.plot(t_TPs[3],cell2_TP4, linewidth = 0.25, color = 'sandybrown')
     axTP4_cell2.set_ylim(3.1,4.1)
     axTP4_cell2.axis('off')
-    axTP4_cell3.plot(t_TP4,cell3_TP4, linewidth = 0.25, color = 'darkkhaki')
+    axTP4_cell3.plot(t_TPs[3],cell3_TP4, linewidth = 0.25, color = 'darkkhaki')
     axTP4_cell3.set_ylim(2, 3)
     axTP4_cell3.axis('off')
-    axTP4_cell4.plot(t_TP4,cell4_TP4, linewidth = 0.25, color = 'seagreen')
+    axTP4_cell4.plot(t_TPs[3],cell4_TP4, linewidth = 0.25, color = 'seagreen')
     axTP4_cell4.set_ylim(1,2)
     axTP4_cell4.axis('off')
-    axTP4_cell5.plot(t_TP4,cell5_TP4, linewidth = 0.25, color = 'royalblue')
+    axTP4_cell5.plot(t_TPs[3],cell5_TP4, linewidth = 0.25, color = 'royalblue')
     axTP4_cell5.set_ylim(0.2,1.2)
     axTP4_cell5.axis('off')
 
