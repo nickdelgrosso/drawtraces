7b9f34683c99ca7bec1341e5bb0bb55f921049b7
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2019-07-29 22:16:07 +0200

    updated pandas and moved comments

--- scripts/draw_traces.py
+++ scripts/draw_traces.py
@@ -6,31 +6,29 @@ def main():
     # load csv as data frame
     df = pd.read_csv('./example_data/draw_traces/top5_correlated.csv')
 
-    frames_recorded = 6080
     # calculate number of time points
+    frames_recorded = 6080
     ntimepoints = int(len(df) // frames_recorded)
 
     # extract time (t) and fluorescence data for each ROI (cell 1-5)
-    t = df.ix[:,'y0000']
+    t = df['y0000']
 
     # extract data for each time point from all ROIs
     cell_names = ['x0000', 'x0001', 'x0002', 'x0003', 'x0004']
     for timepoint in range(ntimepoints):
         cells_TP = []
         for cell_name in cell_names:
-            cell = df.ix[:, cell_name]
-            cell_TP = cell.ix[timepoint*frames_recorded : (timepoint+1)*frames_recorded -1]
+            cell = df[cell_name]
+            cell_TP = cell.iloc[timepoint*frames_recorded : (timepoint+1)*frames_recorded -1]
             cells_TP.append(cell_TP)
 
-        fig = plt.figure()
+        fig, axes = plt.subplots(nrows=len(cell_names))
         colors = ['firebrick', 'sandybrown', 'darkkhaki', 'seagreen', 'royalblue']
         y_lims = [(3.8, 4.8), (3.3, 4.3), (2, 3), (0.8, 1.8), (-0.2, 0.8)]
-        subplots = [511, 512, 513, 514, 515]
         visibilities = ['off', 'off', 'off', 'off', 'on']
 
-        t_TP = t.ix[timepoint * frames_recorded: (timepoint + 1) * frames_recorded - 1]
-        for cell_TP, color, y_lim, subplot, visibility in zip(cells_TP, colors, y_lims, subplots, visibilities):
-            ax = fig.add_subplot(subplot)
+        t_TP = t.iloc[timepoint * frames_recorded: (timepoint + 1) * frames_recorded - 1]
+        for cell_TP, color, y_lim, ax, visibility in zip(cells_TP, colors, y_lims, axes, visibilities):
             ax.plot(t_TP, cell_TP, linewidth=0.25, color=color)
             ax.set(ylim=y_lim, yticks=[])
             ax.axis(visibility)
