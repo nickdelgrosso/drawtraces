effac655e23937e3d2cc2b5faf95eafb6ab0f137
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-07-30 15:02:14 +0200

    removed some plots

--- draw_traces.py
+++ draw_traces.py
@@ -2,19 +2,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
 # load csv as data frame
-df = pd.read_csv('./top5_correlated.csv')
+df = pd.read_csv("./top5_correlated.csv")
 
 # extract time (t) and fluorescence data for each ROI (cell 1-5)
-t = df.ix[:,'y0000']
-cell1 = df.ix[:,'x0000']
-cell2 = df.ix[:,'x0001']
-cell3 = df.ix[:,'x0002']
-cell4 = df.ix[:,'x0003']
-cell5 = df.ix[:,'x0004']
+t = df.ix[:, "y0000"]
+cell1 = df.ix[:, "x0000"]
+cell2 = df.ix[:, "x0001"]
+cell3 = df.ix[:, "x0002"]
+cell4 = df.ix[:, "x0003"]
+cell5 = df.ix[:, "x0004"]
 
-#extract time (t) for 4 timepoints (TP)
+# extract time (t) for 4 timepoints (TP)
 t_TP1 = t.ix[0:6079]
 t_TP2 = t.ix[6080:12159]
 t_TP3 = t.ix[12160:18239]
@@ -62,28 +61,20 @@ axTP1_cell4 = fig_TP1.add_subplot(514)
 axTP1_cell5 = fig_TP1.add_subplot(515)
 
 
-axTP1_cell1.plot(t_TP1, cell1_TP1, linewidth = 0.25)
-axTP1_cell1.axis('off')
-axTP1_cell2.plot(t_TP1,cell2_TP1, linewidth = 0.25)
-axTP1_cell2.axis('off')
-axTP1_cell3.plot(t_TP1,cell3_TP1, linewidth = 0.25)
-axTP1_cell3.axis('off')
-axTP1_cell4.plot(t_TP1,cell4_TP1, linewidth = 0.25)
-axTP1_cell4.axis('off')
-axTP1_cell5.plot(t_TP1,cell5_TP1, linewidth = 0.25)
-axTP1_cell5.axis('off')
+axTP1_cell1.plot(t_TP1, cell1_TP1, linewidth=0.25)
+axTP1_cell2.plot(t_TP1, cell2_TP1, linewidth=0.25)
+axTP1_cell3.plot(t_TP1, cell3_TP1, linewidth=0.25)
+axTP1_cell4.plot(t_TP1, cell4_TP1, linewidth=0.25)
+axTP1_cell5.plot(t_TP1, cell5_TP1, linewidth=0.25)
 
-
-axTP1_cell5.spines['right'].set_visible(False)
-axTP1_cell5.spines['top'].set_visible(False)
-axTP1_cell5.spines['bottom'].set_position(('outward',10))
+axTP1_cell5.spines["right"].set_visible(False)
+axTP1_cell5.spines["top"].set_visible(False)
+axTP1_cell5.spines["bottom"].set_position(("outward", 10))
 axTP1_cell5.set_yticks([])
 
-#Adjust spacing between subplots
-plt.ylabel('ΔF/F')
-plt.xlabel('# of frames')
+# Adjust spacing between subplots
+plt.ylabel("ΔF/F")
+plt.xlabel("# of frames")
 
 plt.show()
-fig_TP1.savefig('TP1.png')
-
-
+fig_TP1.savefig("TP1.png")
